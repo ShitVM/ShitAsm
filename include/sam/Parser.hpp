c93c33329d988919634984095624c9d6a938fdfa
kmc7468 <kmc7468@naver.com>
2020-03-29 20:01:40 +0900

    Parser::SecondPass 멤버 함수 추가

--- include/sam/Parser.hpp
+++ include/sam/Parser.hpp
@@ -3,12 +3,26 @@
 #include <sam/Assembly.hpp>
 
 #include <cstddef>
+#include <cstdint>
 #include <fstream>
+#include <optional>
 #include <ostream>
 #include <string>
+#include <variant>
+
+namespace sam {
+	struct Type final {
+		sgn::Type ElementType;
+		std::string ElementTypeName;
+		std::optional<std::uint64_t> ElementCount;
+	};
+}
 
 namespace sam {
 	class Parser final {
+	private:
+		using Number = std::variant<bool, std::uint32_t, std::uint64_t, double>;
+
 	private:
 		Assembly& m_Assembly;
 		std::ifstream m_ReadStream;
@@ -32,12 +46,27 @@ namespace sam {
 
 	private:
 		bool FirstPass();
+		bool SecondPass();
 
 		bool IgnoreComment();
 		bool IsValidIdentifier(const std::string& identifier);
+		sgn::Type GetType(const std::string& name);
+		template<typename F>
+		bool IsValidIntegerLiteral(const std::string& literal, std::string& literalMut, F&& function);
 
 		bool ParseStructure();
+		bool ParseField();
 		bool ParseFunction(bool isProcedure);
 		bool ParseLabel();
+
+		std::optional<Type> ParseType(std::string& line);
+		Number ParseNumber(std::string& line);
+		template<typename F>
+		Number ParseInteger(const std::string& literal, std::string& literalMut, bool isNegative, int base, F&& function);
+		Number ParseBinInteger(const std::string& literal, std::string& literalMut, bool isNegative);
+		Number ParseOctInteger(const std::string& literal, std::string& literalMut, bool isNegative);
+		Number ParseDecInteger(const std::string& literal, std::string& literalMut, bool isNegative);
+		Number ParseHexInteger(const std::string& literal, std::string& literalMut, bool isNegative);
+		Number ParseDecimal(const std::string& literal, std::string& literalMut, bool isNegative);
 	};
 }
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -4,9 +4,13 @@
 #include <sam/String.hpp>
 #include <sgn/ByteFile.hpp>
 #include <sgn/Structure.hpp>
+#include <svm/Type.hpp>
 
 #include <algorithm>
 #include <cctype>
+#include <limits>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 namespace sam {
@@ -28,11 +32,11 @@ namespace sam {
 				std::transform(mnemonic.begin(), mnemonic.end(), mnemonic.begin(), [](char c) { return static_cast<char>(std::tolower(c)); });
 
 				if (mnemonic == "struct") {
-					hasError = !ParseStructure();
+					hasError |= !ParseStructure();
 				} else if (mnemonic == "proc" || mnemonic == "func") {
-					hasError = !ParseFunction(mnemonic == "proc");
+					hasError |= !ParseFunction(mnemonic == "proc");
 				} else {
-					hasError = !ParseLabel();
+					hasError |= !ParseLabel();
 				}
 			}
 		}
@@ -40,6 +44,33 @@ namespace sam {
 		if (!m_Assembly.HasFunction("entrypoint")) {
 			m_ErrorStream << "Error: There is no 'entrypoint' procedure.\n";
 			hasError = true;
+		}
+
+		return !hasError;
+	}
+	bool Parser::SecondPass() {
+		std::size_t structInx = 0;
+		std::size_t fieldInx = 0;
+
+		bool hasError = false;
+
+		while (std::getline(m_ReadStream, m_Line) && ++m_LineNum) {
+			if (IgnoreComment()) continue;
+
+			if (m_Line.find(':') != std::string::npos) {
+				std::string mnemonic = ReadBeforeSpace(m_Line);
+				std::transform(mnemonic.begin(), mnemonic.end(), mnemonic.begin(), [](char c) { return static_cast<char>(std::tolower(c)); });
+
+				if (mnemonic == "proc" || mnemonic == "func") {
+					m_CurrentStructure.clear();
+				} else if (mnemonic == "struct") {
+					m_CurrentStructure = m_Assembly.Structures[structInx++].Name;
+					fieldInx = 0;
+				}
+				continue;
+			} else if (m_CurrentStructure.empty()) continue;
+
+			hasError |= !ParseField();
 		}
 
 		return !hasError;
@@ -60,6 +91,36 @@ namespace sam {
 			if (std::isspace(c)) return false;
 		}
 
+		return true;
+	}
+	sgn::Type Parser::GetType(const std::string& name) {
+		static const std::unordered_map<std::string, sgn::Type> fundamental = {
+			{ "int", sgn::IntType },
+			{ "long", sgn::LongType },
+			{ "double", sgn::DoubleType },
+			{ "pointer", sgn::PointerType },
+			{ "gcpointer", sgn::GCPointerType },
+		};
+
+		const auto iter = fundamental.find(name);
+		if (iter != fundamental.end()) return svm::GetType(sgn::Structures(), iter->second->Code);
+
+		const auto structIter = m_Assembly.FindStructure(name);
+		if (structIter != m_Assembly.Structures.end()) return m_Assembly.ByteFile.GetStructureInfo(structIter->Index)->Type;
+		else return nullptr;
+	}
+	template<typename F>
+	bool Parser::IsValidIntegerLiteral(const std::string& literal, std::string& literalMut, F&& function) {
+		for (std::size_t i = 0; i < literalMut.size(); ++i) {
+			if (i != 0 && i != literalMut.size() - 1 && literalMut[i] == ',') {
+				literalMut.erase(literalMut.begin() + i);
+				--i;
+			} else if (i == literalMut.size() - 1 && (literalMut[i] == 'i' || literalMut[i] == 'l')) continue;
+			else if (function(literalMut[i])) {
+				ERROR << "Invalid integer literal '" << literal << "'.\n";
+				return false;
+			}
+		}
 		return true;
 	}
 
@@ -91,6 +152,31 @@ namespace sam {
 
 		const sgn::StructureIndex index = m_Assembly.ByteFile.AddStructure();
 		m_Assembly.Structures.push_back(Structure{ std::move(name), index });
+
+		return !hasError;
+	}
+	bool Parser::ParseField() {
+		bool hasError = false;
+
+		const auto type = ParseType(m_Line);
+		if (!type) return false;
+		else if (!type->ElementCount || *type->ElementCount == 0) {
+			ERROR << "Array in structure must have length.\n";
+			hasError = true;
+		}
+
+		std::string name = ReadBeforeSpecialChar(m_Line); Trim(name);
+		if (!IsValidIdentifier(name)) {
+			ERROR << "Invalid field name '" << name << "'.\n";
+			hasError = true;
+		} else if (type->ElementType == nullptr) {
+			ERROR << "Nonexistent type name '" << type->ElementTypeName << "'.\n";
+			return false;
+		}
+
+		sgn::StructureIndex structIndex = m_Assembly.GetStructure(m_CurrentStructure).Index;
+		sgn::StructureInfo* const structure = m_Assembly.ByteFile.GetStructureInfo(structIndex);
+		structure->AddField(type->ElementType, type->ElementCount.value_or(0));
 
 		return !hasError;
 	}
@@ -180,5 +266,158 @@ namespace sam {
 		currentFunction.Labels.push_back(Label{ std::move(name) });
 
 		return !hasError;
+	}
+
+	std::optional<Type> Parser::ParseType(std::string& line) {
+		bool hasError = false;
+
+		std::string typeName;
+		if (line.find('[') != std::string::npos) {
+			std::string temp = ReadBeforeChar(line, '['); Trim(typeName);
+			typeName = ReadBeforeSpace(temp); Trim(typeName);
+			if (!temp.empty()) {
+				ERROR << "Unexcepted characters after type name '" << typeName << "'.\n";
+				hasError = true;
+			}
+		} else {
+			typeName = ReadBeforeSpace(line); Trim(typeName);
+		}
+
+		const sgn::Type type = GetType(typeName);
+		if (line.empty() || line.front() != '[') return Type{ type, std::move(typeName) };
+
+		std::optional<std::uint64_t> count;
+		std::string countStr = ReadBeforeChar(line, ']'); countStr.erase(countStr.begin()); Trim(countStr);
+
+		const auto countVar = ParseNumber(countStr);
+		if (std::holds_alternative<bool>(countVar) && std::get<bool>(countVar)) {
+			hasError = true;
+		} else if (std::holds_alternative<double>(countVar)) {
+			ERROR << "Array's length must be integer.\n";
+			count = static_cast<std::uint64_t>(std::get<double>(countVar));
+			hasError = true;
+		} else {
+			std::visit([&count](auto v) mutable {
+				count = static_cast<std::uint64_t>(v);
+			}, countVar);
+		}
+
+		if (line.empty()) {
+			if (count) {
+				ERROR << "Unexcepted end-of-file after array's length.\n";
+			} else {
+				ERROR << "Unexcpeted end-of-file after '['.\n";
+			}
+			hasError = true;
+		} else if (line.front() != ']') {
+			if (count) {
+				ERROR << "Excepted ']' after array's length.\n";
+			} else {
+				ERROR << "Excpeted ']' after '['.\n";
+			}
+			hasError = true;
+		}
+
+		if (hasError) return std::nullopt;
+		else return Type{ type, std::move(typeName), count };
+	}
+	Parser::Number Parser::ParseNumber(std::string& line) {
+		std::string literal = ReadBeforeSpace(line); Trim(literal);
+		if (literal.empty()) return false;
+
+		std::string literalMut = literal;
+		const bool isNegative = literal.front() == '-';
+		if (isNegative || literal.front() == '+') {
+			if (literal.size() == 1) {
+				literal = ReadBeforeSpace(line); Trim(literal);
+				if (literal.empty()) {
+					ERROR << "Excepted number literal after '" << literalMut.front() << "'.\n";
+					return true;
+				}
+				literalMut = literal;
+			}
+		}
+
+		if (literalMut.front() == '0') {
+			if (literalMut.size() == 1) return 0u;
+			switch (literalMut[1]) {
+			case 'x':
+			case 'X':
+				literalMut.erase(literalMut.begin(), literalMut.begin() + 2);
+				return ParseHexInteger(literal, literalMut, isNegative);
+
+			case 'b':
+			case 'B':
+				literalMut.erase(literalMut.begin(), literalMut.begin() + 2);
+				return ParseBinInteger(literal, literalMut, isNegative);
+
+			default:
+				return ParseOctInteger(literal, literalMut, isNegative);
+			}
+		} else return ParseDecInteger(literal, literalMut, isNegative);
+	}
+	template<typename F>
+	Parser::Number Parser::ParseInteger(const std::string& literal, std::string& literalMut, bool isNegative, int base, F&& function) {
+		if (literalMut.find('.') != std::string::npos) {
+			ERROR << "Invalid integer literal '" << literal << "'.\n";
+			return false;
+		} else if (!IsValidIntegerLiteral(literal, literalMut, std::forward<F>(function))) return false;
+
+		const unsigned long long abs = std::stoull(literalMut, nullptr, base);
+		if (isNegative) {
+			if (literalMut.back() == 'i') {
+				if (abs > -std::numeric_limits<std::int32_t>::min()) {
+					WARNING << "Overflowed integer literal '" << literalMut << "'.\n";
+				}
+				return static_cast<std::uint32_t>(-abs);
+			} else {
+				if (abs > -std::numeric_limits<std::int64_t>::min()) {
+					WARNING << "Overflowed integer literal '" << literalMut << "'.\n";
+				}
+				return static_cast<std::uint64_t>(-abs);
+			}
+		} else {
+			if (literalMut.back() == 'i') {
+				if (abs > std::numeric_limits<std::int32_t>::max()) {
+					WARNING << "Overflowed integer literal '" << literalMut << "'.\n";
+				}
+				return static_cast<std::uint32_t>(abs);
+			} else return static_cast<std::uint64_t>(abs);
+		}
+	}
+	Parser::Number Parser::ParseBinInteger(const std::string& literal, std::string& literalMut, bool isNegative) {
+		return ParseInteger(literal, literalMut, isNegative, 2, [](char c) {
+			return !(c == '0' || c == '1');
+		});
+	}
+	Parser::Number Parser::ParseOctInteger(const std::string& literal, std::string& literalMut, bool isNegative) {
+		if (literalMut.find('.') != std::string::npos) return ParseDecimal(literal, literalMut, isNegative);
+		else return ParseInteger(literal, literalMut, isNegative, 8, [](char c) {
+			return !(c <= '0' && c <= '7');
+		});
+	}
+	Parser::Number Parser::ParseDecInteger(const std::string& literal, std::string& literalMut, bool isNegative) {
+		if (literalMut.find('.') != std::string::npos) return ParseDecimal(literal, literalMut, isNegative);
+		else return ParseInteger(literal, literalMut, isNegative, 10, [](char c) {
+			return !(std::isdigit(c));
+		});
+	}
+	Parser::Number Parser::ParseHexInteger(const std::string& literal, std::string& literalMut, bool isNegative) {
+		return ParseInteger(literal, literalMut, isNegative, 16, [](char c) {
+			return !(std::isdigit(c) ||
+					 'a' <= c && c <= 'f' ||
+					 'A' <= c && c <= 'F');
+		});
+	}
+	Parser::Number Parser::ParseDecimal(const std::string& literal, std::string& literalMut, bool isNegative) {
+		const std::size_t dot = literalMut.find('.');
+		if (literalMut.find('.', dot + 1) != std::string::npos) {
+			ERROR << "Invalid decimal literal '" << literal << "'.\n";
+			return true;
+		}
+
+		const double abs = std::stod(literalMut);
+		if (isNegative) return -abs;
+		else return abs;
 	}
 }
