62377b09062736f118a2637f1756d28562137a8f
kmc7468 <kmc7468@naver.com>
2020-03-31 10:55:21 +0900

    Lexer 클래스 추가

--- include/sam/Lexer.hpp
+++ include/sam/Lexer.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <cstddef>
+#include <istream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace sam {
 	enum class TokenType {
 		None,
-		WhiteSpace,
 		NewLine,
 
 		Identifier,
@@ -16,6 +18,9 @@ namespace sam {
 		DecInteger,
 		HexInteger,
 		Decimal,
+
+		Plus,					// +
+		Minus,					// -
 
 		Character,
 		String,
@@ -34,6 +39,41 @@ namespace sam {
 		TokenType Type = TokenType::None;
 		std::size_t Line = 0;
 
+		Token(TokenType type, std::size_t line) noexcept;
 		Token(std::string word, TokenType type, std::size_t line) noexcept;
+	};
+}
+
+namespace sam {
+	class Lexer final {
+	private:
+		std::istream& m_InputStream;
+		std::ostringstream m_ErrorStream;
+
+		std::string m_Line;
+		std::size_t m_LineNum = 0;
+
+		std::vector<Token> m_Result;
+		bool m_HasError = false;
+
+	public:
+		Lexer(std::istream& inputStream) noexcept;
+		Lexer(const Lexer&) = delete;
+		~Lexer() = default;
+
+	public:
+		Lexer& operator=(const Lexer&) = delete;
+		bool operator==(const Lexer&) = delete;
+		bool operator!=(const Lexer&) = delete;
+
+	public:
+		void Lex();
+
+	private:
+		void LexSpecial(char firstByte);
+
+		bool IgnoreComment() noexcept;
+
+		char GetNextByte(std::size_t i) const noexcept;
 	};
 }
--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,8 +1,69 @@
 #include <sam/Lexer.hpp>
 
+#include <sam/Encoding.hpp>
+#include <sam/String.hpp>
+
+#include <cctype>
 #include <utility>
 
 namespace sam {
+	Token::Token(TokenType type, std::size_t line) noexcept
+		: Type(type), Line(line) {}
 	Token::Token(std::string word, TokenType type, std::size_t line) noexcept
 		: Word(std::move(word)), Type(type), Line(line) {}
+}
+
+namespace sam {
+	Lexer::Lexer(std::istream& inputStream) noexcept
+		: m_InputStream(inputStream) {}
+
+#define ERROR (m_HasError = true, m_ErrorStream) << "    Error: Line " << m_LineNum << ", "
+
+	void Lexer::Lex() {
+		while (std::getline(m_InputStream, m_Line) && ++m_LineNum) {
+			if (IgnoreComment()) continue;
+			for (std::size_t i = 0; i < m_Line.size(); ++i) {
+				const char firstByte = m_Line[i];
+				if (IsSpecial(firstByte)) {
+					LexSpecial(firstByte);
+				}
+			}
+		}
+	}
+
+	void Lexer::LexSpecial(char firstByte) {
+#define CASE(c, e) case c: m_Result.emplace_back(TokenType:: e, m_Line); return
+		switch (firstByte) {
+		CASE('+', Plus);
+		CASE('-', Minus);
+
+		CASE(':', Colon);
+		CASE('.', Dot);
+		CASE(',', Comma);
+		CASE('[', LeftBracket);
+		CASE(']', RightBracket);
+		CASE('(', LeftParenthesis);
+		CASE(')', RightParenthesis);
+		default:
+			ERROR << "Unusable character '" << firstByte << "'.\n";
+			return;
+		}
+#undef CASE
+	}
+
+	bool Lexer::IgnoreComment() noexcept {
+		if (const auto commentBegin = m_Line.find(';'); commentBegin != std::string::npos) {
+			m_Line.erase(m_Line.begin() + commentBegin, m_Line.end());
+		}
+
+		Trim(m_Line);
+		return m_Line.empty();
+	}
+
+	char Lexer::GetNextByte(std::size_t i) const noexcept {
+		if (i >= m_Line.size()) return 0;
+		else return m_Line[i];
+	}
+
+#undef ERROR
 }
