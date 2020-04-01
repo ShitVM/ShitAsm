aa761e747e7d634c9c000a6d5b49f8816ee26530
kmc7468 <kmc7468@naver.com>
2020-04-01 23:55:58 +0900

    Token::Data 멤버 변수 추가

--- include/sam/Lexer.hpp
+++ include/sam/Lexer.hpp
@@ -4,6 +4,7 @@
 #include <istream>
 #include <sstream>
 #include <string>
+#include <variant>
 #include <vector>
 
 namespace sam {
@@ -27,37 +28,44 @@ namespace sam {
 
 		Colon,					// :
 		Dot,					// .
-		Comma,					// ,
 		LeftBracket,			// [
 		RightBracket,			// ]
 		LeftParenthesis,		// (
 		RightParenthesis,		// )
 	};
 
+	using Char = std::variant<char, char16_t, char32_t>;
+	using String = std::variant<std::string, std::u16string, std::u32string>;
+	using Number = std::variant<std::uint32_t, std::uint64_t, double>;
+	using TokenData = std::variant<std::monostate, Char, String, Number>;
+
 	struct Token final {
 		std::string Word;
+		TokenData Data;
 		TokenType Type = TokenType::None;
 		std::size_t Line = 0;
 
 		Token(TokenType type, std::size_t line) noexcept;
-		Token(std::string word, TokenType type, std::size_t line) noexcept;
+		Token(std::string word, TokenData data, TokenType type, std::size_t line) noexcept;
 	};
 }
 
 namespace sam {
 	class Lexer final {
 	private:
+		std::string m_Path;
 		std::istream& m_InputStream;
 		std::ostringstream m_ErrorStream;
 
 		std::string m_Line;
 		std::size_t m_LineNum = 0;
+		std::size_t m_Column = 0;
 
 		std::vector<Token> m_Result;
 		bool m_HasError = false;
 
 	public:
-		Lexer(std::istream& inputStream) noexcept;
+		Lexer(const std::string& path, std::istream& inputStream) noexcept;
 		Lexer(const Lexer&) = delete;
 		~Lexer() = default;
 
@@ -68,12 +76,13 @@ namespace sam {
 
 	public:
 		void Lex();
+		bool HasError() const noexcept;
+		std::string GetMessages() const;
 
 	private:
-		void LexSpecial(char firstByte);
-
 		bool IgnoreComment() noexcept;
-
 		char GetNextByte(std::size_t i) const noexcept;
+
+		void LexSpecial(char firstByte);
 	};
 }
--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -9,46 +9,35 @@
 namespace sam {
 	Token::Token(TokenType type, std::size_t line) noexcept
 		: Type(type), Line(line) {}
-	Token::Token(std::string word, TokenType type, std::size_t line) noexcept
-		: Word(std::move(word)), Type(type), Line(line) {}
+	Token::Token(std::string word, TokenData data, TokenType type, std::size_t line) noexcept
+		: Word(std::move(word)), Data(data), Type(type), Line(line) {}
 }
 
 namespace sam {
-	Lexer::Lexer(std::istream& inputStream) noexcept
-		: m_InputStream(inputStream) {}
+	Lexer::Lexer(const std::string& path, std::istream& inputStream) noexcept
+		: m_Path(path), m_InputStream(inputStream) {}
 
-#define ERROR (m_HasError = true, m_ErrorStream) << "    Error: Line " << m_LineNum << ", "
+#define MESSAGEBASE m_ErrorStream << "In file '" << m_Path << "':\n    "
+#define INFO MESSAGEBASE << "Info: Line " << m_LineNum << ", "
+#define WARNING MESSAGEBASE << "Warning: Line " << m_LineNum << ", "
+#define ERROR (m_HasError = true, MESSAGEBASE) << "Error: Line " << m_LineNum << ", "
 
 	void Lexer::Lex() {
 		while (std::getline(m_InputStream, m_Line) && ++m_LineNum) {
 			if (IgnoreComment()) continue;
-			for (std::size_t i = 0; i < m_Line.size(); ++i) {
-				const char firstByte = m_Line[i];
+			for (m_Column = 0; m_Column < m_Line.size();) {
+				const char firstByte = m_Line[m_Column];
 				if (IsSpecial(firstByte)) {
 					LexSpecial(firstByte);
 				}
 			}
 		}
 	}
-
-	void Lexer::LexSpecial(char firstByte) {
-#define CASE(c, e) case c: m_Result.emplace_back(TokenType:: e, m_Line); return
-		switch (firstByte) {
-		CASE('+', Plus);
-		CASE('-', Minus);
-
-		CASE(':', Colon);
-		CASE('.', Dot);
-		CASE(',', Comma);
-		CASE('[', LeftBracket);
-		CASE(']', RightBracket);
-		CASE('(', LeftParenthesis);
-		CASE(')', RightParenthesis);
-		default:
-			ERROR << "Unusable character '" << firstByte << "'.\n";
-			return;
-		}
-#undef CASE
+	bool Lexer::HasError() const noexcept {
+		return m_HasError;
+	}
+	std::string Lexer::GetMessages() const {
+		return m_ErrorStream.str();
 	}
 
 	bool Lexer::IgnoreComment() noexcept {
@@ -59,11 +48,30 @@ namespace sam {
 		Trim(m_Line);
 		return m_Line.empty();
 	}
-
 	char Lexer::GetNextByte(std::size_t i) const noexcept {
 		if (i >= m_Line.size()) return 0;
 		else return m_Line[i];
 	}
 
-#undef ERROR
+	void Lexer::LexSpecial(char firstByte) {
+#define CASE(e, c) case c: m_Result.emplace_back(TokenType:: e, m_LineNum); break
+		switch (firstByte) {
+		CASE(Plus, '+');
+		CASE(Minus, '-');
+
+		CASE(Colon, ':');
+		CASE(Dot, '.');
+		CASE(LeftBracket, '[');
+		CASE(RightBracket, ']');
+		CASE(LeftParenthesis, '(');
+		CASE(RightParenthesis, ')');
+
+		default:
+			ERROR << "Unusable character '" << firstByte << "'.\n";
+			break;
+		}
+#undef CASE
+
+		++m_Column;
+	}
 }
